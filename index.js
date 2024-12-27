77a96be292eb9f7fdce212df5aeedb2fddc094e8
Jesdhy <pinchajessy529@gmail.com>
2024-12-27 18:52:26 -0500

    style: :lipstick: Add interface
    Add form to enter pet data

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 4547;
-const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConfig');
 const createDogRoutes = require('./routes/createDogRoutes');
-
-app.use('/', createDogRoutes);
+const path = require('path');
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 connectDB();
 
-app.use('/api/CreateDog', createDogRoutes);
+app.use('/api/dogs', createDogRoutes);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
